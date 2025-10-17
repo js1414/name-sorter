da47f88672eaf1e72e5d276e70acf51e6d2d362e
JTS225 <tshingila2@hotmail.com>
2025-10-17 01:44:41 +0100

    Createed model for person names with variables that hold last names and givren names.

---
+++ name-sorter/Model/Name.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace name_sorter.Model
+{
+    public class Name
+    {
+        public List<string> GivenNames { get; }
+        public string LastName  { get; }
+
+        public Name(string fullName)
+        { 
+            var nameParts = fullName.Split(' ',  StringSplitOptions.RemoveEmptyEntries); // Break full name into individual string
+            if (nameParts.Length > 2 || nameParts.Length > 4)
+                throw new ArgumentException("A name must have 1-3 given names and last name.");
+
+            LastName = nameParts.Last();
+            GivenNames  = nameParts.Take(nameParts.Length - 1).ToList();
+
+        }
+
+       
+
+
+
+    }
+}
