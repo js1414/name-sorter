ae3402a616915c90f5c3540cf7aeb75eb21411ad
JTS225 <tshingila2@hotmail.com>
2025-10-17 12:34:52 +0100

    feat: set reading path for for file reading and handled error using try catch to check for empty path or file no found.

--- name-sorter/Services/FileNameReader.cs
+++ name-sorter/Services/FileNameReader.cs
@@ -6,8 +6,30 @@ using System.Threading.Tasks;
 
 namespace name_sorter.Service
 {
-    public class FileNameReader
+    public class FileNameReader : INameReader
     {
+        private readonly string _filePath;
+        public FileNameReader(string filePath) => _filePath = filePath;
 
+        public IEnumerable<string> ReadNames()
+        {
+            //return File.ReadAllLines(_filePath).Where(line => !string.IsNullOrWhiteSpace(line));
+
+            try
+            {
+                if (string.IsNullOrWhiteSpace(_filePath))
+                    throw new ArgumentException("File path cannot be null or empty.", nameof(_filePath));
+
+                if (!File.Exists(_filePath))
+                    throw new FileNotFoundException($"Input file not found: {_filePath}");
+
+            }
+            catch { }
+
+            // returns a sequence of non-empty lines from the file.
+            return File.ReadAllLines(_filePath).Where(line => !string.IsNullOrWhiteSpace(line));
+
+
+        }
     }
 }
