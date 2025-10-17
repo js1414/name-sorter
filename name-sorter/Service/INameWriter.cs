84902377db1d398b1d1031f5c025254473c7c407
JTS225 <tshingila2@hotmail.com>
2025-10-17 01:49:46 +0100

    Created a folder service to store all service files

---
+++ name-sorter/Service/INameWriter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace name_sorter.Service
+{
+    public interface INameWriter
+    {
+    }
+}
